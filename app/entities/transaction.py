3e594a108cbca7d92f67549af95b29059f569f03
Eric Costa <ericosta.dev@gmail.com>
2023-07-02 23:20:52 -0300

    feat: add validations to each field for Transaction

--- app/entities/transaction.py
+++ app/entities/transaction.py
@@ -30,9 +30,48 @@ class Transaction:
         date: datetime,
         note: str,
     ):
-        self.id = id
-        self.category = category
-        self.type = type
-        self.value = value
-        self.date = date
-        self.note = note
+        self.id = self.validate_id(id)
+        self.category = self.validate_category(category)
+        self.type = self.validate_type(type)
+        self.value = self.validate_value(value)
+        self.date = self.validate_date(date)
+        self.note = self.validate_note(note)
+
+    def __str__(self):
+        return f"Transaction {self.id}: {self.category.name} - {self.value} - {self.date}"
+
+    @staticmethod
+    def validate_id(id: int):
+        if not isinstance(id, int) or id <= 0:
+            raise TypeError("Transaction id must be an positive integer")
+        return id
+
+    @staticmethod
+    def validate_category(category: Category):
+        if not isinstance(category, Category) or category.id <= 0:
+            raise TypeError("Transaction category must be a Category entity")
+        return category
+
+    @staticmethod
+    def validate_type(type: TypeTransaction):
+        if not isinstance(type, TypeTransaction) or type not in TypeTransaction:
+            raise TypeError("Transaction type must be a TypeTransaction entity")
+        return type
+
+    @staticmethod
+    def validate_value(value: float):
+        if not isinstance(value, float) or value < 0:
+            raise TypeError("Transaction value must be a positive float")
+        return value
+
+    @staticmethod
+    def validate_date(date: datetime):
+        if not isinstance(date, datetime):
+            raise TypeError("Transaction date must be a datetime entity")
+        return date
+
+    @staticmethod
+    def validate_note(note: str):
+        if not isinstance(note, str):
+            raise TypeError("Transaction note must be a string")
+        return note
